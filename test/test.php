46612ccd43b0817d996c71fa2ba48660092b82f3
Manish Sharma <manish@test.com>
2023-11-28 06:48:31 -0500

    test demo

---
+++ test/test.php
@@ -0,0 +1,11 @@
+<?php
+$arr1 = ['EVO 80'];
+$arr2 = ['Delevan'];
+$notfound = [];
+foreach ($arr1 as $key => $value) {
+   if(!in_array($value,$arr2)) {
+    $notfound[] = $value;
+   }
+}
+echo implode(',',array_unique($notfound));
+?>
